2e054c05cc5150883d01c5a9149ae4225a314002
Bastian Inuk Christensen <jonalist@me.com>
2019-07-03 22:03:24 +0200

    Initial Commit

---
+++ AudioUnit over IP/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AudioUnit over IP
+//
+//  Created by Bastian Inuk Christensen on 03/07/2019.
+//  Copyright © 2019 Inuk Entertainment. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ AudioUnit over IPTests/AudioUnit_over_IPTests.swift
@@ -0,0 +1,34 @@
+//
+//  AudioUnit_over_IPTests.swift
+//  AudioUnit over IPTests
+//
+//  Created by Bastian Inuk Christensen on 03/07/2019.
+//  Copyright © 2019 Inuk Entertainment. All rights reserved.
+//
+
+import XCTest
+@testable import AudioUnit_over_IP
+
+class AudioUnit_over_IPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
